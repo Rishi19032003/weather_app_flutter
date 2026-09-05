1b75c5122e17c8cf37b94c3d25a966656c2aeec9
Rishi19032003 <bvrishi9@gmail.com>
2026-09-05 20:03:45 +0530

    started with the layout of the weather app

--- lib/weather_screen.dart
+++ lib/weather_screen.dart
@@ -5,6 +5,44 @@ class WeatherScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Weather App',
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint('refresh');
+            },
+            icon: Icon(Icons.refresh),
+          ),
+        ],
+        backgroundColor: const Color.fromARGB(255, 137, 131, 131),
+      ),
+      body: Column(
+        children: [
+          //main card
+          Card(
+            child: Column(
+              children: [
+                Text(
+                  '300 ° K',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 20),
+          //Weather Forecast
+          const Placeholder(fallbackHeight: 150),
+          const SizedBox(height: 20),
+          //Additional Information
+          const Placeholder(fallbackHeight: 150),
+        ],
+      ),
+    );
   }
 }
